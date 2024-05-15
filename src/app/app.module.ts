efeb96a3f370cee69cb7670a8aa07ebd4edb40df
Antonio Gomez Janus <antogo8768@gmail.com>
2024-05-15 09:05:42 -0500

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductoListaComponent } from './producto-lista/producto-lista.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AgregarProductoComponent } from './agregar-producto/agregar-producto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductoListaComponent,
+    AgregarProductoComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [
